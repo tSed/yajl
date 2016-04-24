250598a4f0cbb26a5dd86b0a3a08ca4f93c43556
Samuel Martin <s.martin49@gmail.com>
2016-04-24 18:45:27 +0200

    Link libyajl{,_s} with libm when isnan is not brought by the libc
    Check whether isnan is provided by the libc library, otherwise make sure
    yajl libraries are link against libm.
    
    Note that setting libm as PUBLIC link libraries enable the transitivity
    [1, 2]; therefore it will be automatically passed to target linking
    against libyajl{,_s}.
    
    This patch also makes sure the link libraries will appear in the yajl.pc
    file.
    
    [1] https://cmake.org/cmake/help/v3.5/command/target_link_libraries.html
    [2] https://cmake.org/cmake/help/v3.5/manual/cmake-buildsystem.7.html#target-usage-requirements
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>

--- src/yajl.pc.cmake
+++ src/yajl.pc.cmake
@@ -6,4 +6,4 @@ Name: Yet Another JSON Library
 Description: A Portable JSON parsing and serialization library in ANSI C
 Version: ${YAJL_MAJOR}.${YAJL_MINOR}.${YAJL_MICRO}
 Cflags: -I${dollar}{includedir}
-Libs: -L${dollar}{libdir} -lyajl
+Libs: -L${dollar}{libdir} -lyajl ${yajl_lib_link}
